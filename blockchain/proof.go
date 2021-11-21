d66956ec0f9e82b48694549d9acf9515fffebf26
gorewiczMark <gorewicz.mark@gmail.com>
2021-11-21 16:58:10 -0700

    added functionality for Proof of Work

---
+++ blockchain/proof.go
@@ -0,0 +1,85 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+const Difficulty = 12
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func (p *ProofOfWork) InitNonce(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			p.Block.PrevHash,
+			p.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (p *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
+	nonce := 0
+
+	for nonce < math.MaxInt64 {
+		data := p.InitNonce(nonce)
+		hash := sha256.Sum256(data)
+
+		fmt.Printf("\r%x", hash)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(p.Target) == -1 {
+			break
+		}
+
+		nonce++
+	}
+	fmt.Println()
+
+	return nonce, hash[:]
+}
+
+func (p *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := p.InitNonce(p.Block.Nonce)
+
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(p.Target) == -1
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
