dca9024492c2a629ae9f2ec2bdad85a82e5c54d6
MarkGorewicz <gorewicz.mark@gmail.com>
2022-08-26 08:59:33 -0700

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -2,7 +2,13 @@
 
 echo $GITHUB_REPOSITORY
 type=$GITHUB_EVENT_NAME
+file=$GITHUB_EVENT_PATH
 if [ $type == "pull_request" ]; then
   echo $GITHUB_EVENT.NUMBER
+  num=$(cat $file | jr -r .pull_request.changed_files
+  echo $num
+  if [ $num > 1 ]; then
+    echo fail
+  fi
 fi
-cat $GITHUB_EVENT_PATH
+
